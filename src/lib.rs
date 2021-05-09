f74db488d6d830fa7e7a4406088a97bf1d11189b
ferhatgec <ferhat2gecdogan_fix_me5@protonmail.com>
2021-05-09 22:05:56 +0300

    Add: CPU structure.

--- src/lib.rs
+++ src/lib.rs
@@ -15,24 +15,30 @@
 
 pub mod system;
 pub mod helpers;
+pub mod cpu;
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn system_infos() {
-        let infos = crate::system::sys::init();
+        let infos  = crate::system::sys::init();
+        let cpu_infos= crate::cpu::cpu::init();
 
         println!(
             "\
             OS      : {}\n\
             Kernel  : {}\n\
             Username: {}\n\
-            Hostname: {}",
+            Hostname: {}\n\
+            --------\n\
+            CPU     : {}",
 
             infos.os_name,
             infos.kernel,
             infos.username,
-            infos.hostname
+            infos.hostname,
+
+            cpu_infos.model_name
         );
     }
 }
